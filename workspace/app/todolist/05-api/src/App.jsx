92813e14706b75d321d40061beac33d50576b618
Ansinnn <siun2009@gmail.com>
2024-11-27 01:38:54 +0900

    ✨ app/todolist 05-api 추가

---
+++ workspace/app/todolist/05-api/src/App.jsx
@@ -0,0 +1,16 @@
+import { RouterProvider } from "react-router-dom"
+// import router from "./routes"
+import router from "./routes-lazy"
+import { Suspense } from "react"
+
+
+function App() {
+  return (
+    <Suspense fallback={ <div>Loading...</div> }>
+      <RouterProvider router={router} future={{ v7_fetcherPersist: true }}/>
+    </Suspense>
+    
+  )
+}
+
+export default App
