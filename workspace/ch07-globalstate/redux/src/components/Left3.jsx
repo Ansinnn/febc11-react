b84b152d17c64b6106a0ec14e3ac0d536d428ff6
Ansinnn <siun2009@gmail.com>
2024-12-01 21:00:40 +0900

    ✅ 07-globalstate redux 수정

--- workspace/ch07-globalstate/redux/src/components/Left3.jsx
+++ workspace/ch07-globalstate/redux/src/components/Left3.jsx
@@ -1,13 +1,24 @@
 import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 function Left3() {
   useEffect(()=>{
     console.log('      # Left3 렌더링.');
   });
+
+  // redux
+  // useSelector() 훅으로 스토어 접근(자동으로 구독이 됨)
+  // const state = useSelector(state => state);
+  // const count = useSelector(state => state.count);
+  // const date = useSelector(state => state.date);
+
+  // redux-toolkit
+  const count = useSelector(state => state.counterStore.count);
+
   return (
     <div>
       <h3>Left3</h3>
-      <span>0</span>
+      <span>{ count }</span>
     </div>
   );
 }
